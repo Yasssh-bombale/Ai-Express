c11c9aec1360fb404f6f34536682d8af4f8dce5e
Yash Vijay Bombale <yashbombale@gmail.com>
2024-03-29 20:04:13 +0530

    Add clerk authentication sign-in and sign-up page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
-  return <p>welcome to dashboard page (Protected)</p>;
+  return (
+    <>
+      <p>welcome to dashboard page (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </>
+  );
 };
 
 export default DashboardPage;
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
-  return <p>Landing page (Unprotected)</p>;
+  return (
+    <>
+      <p>Landing page (Unprotected)</p>
+      <Link href={"/sign-in"}>
+        <Button>Sign-in</Button>
+      </Link>
+      <Link href={"/sign-up"}>
+        <Button>Sign-up</Button>
+      </Link>
+    </>
+  );
 };
 
 export default LandingPage;
