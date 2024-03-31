923fe86831f8dc832165c48181bd0a63b0ea19b3
Yash Vijay Bombale <yashbombale@gmail.com>
2024-03-31 20:52:22 +0530

    Complete Dashboard page

--- components/Mobile-sidebar.tsx
+++ components/Mobile-sidebar.tsx
@@ -5,8 +5,19 @@ import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import Sidebar from "@/components/Sidebar";
+import { useEffect, useState } from "react";
 
 const Mobilesidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
