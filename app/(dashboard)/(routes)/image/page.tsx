48f70bdad266ad3712c376a78f72321299e2568e
Yash Vijay Bombale <yashbombale@gmail.com>
2024-04-20 16:10:00 +0530

    Complete image generation page functionality

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import * as z from "zod";
+import { useRouter } from "next/navigation";
+import Heading from "@/components/Heading";
+import { Download, Image as Img } from "lucide-react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./constant";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import Empty from "@/components/Empty";
+import Loader from "@/components/Loader";
+
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
+
+const ImagePage = () => {
+  const router = useRouter();
+
+  const [images, setImages] = useState<string[]>([]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setImages([]); //whenever form submits images state will set to empty array[];
+
+      const response = await axios.post("/api/image", values);
+
+      const url = response.data.map((image: { url: string }) => image.url);
+
+      setImages(url);
+
+      form.reset(); // it will clear input after submiting prompt
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Image Generation"
+        description="Convert your prompt into an image."
+        icon={Img}
+        iconColor="text-pink-700"
+        bgColor="bg-pink-700/10"
+      />
+
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6  focus-within:shadow-sm  grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="A picture of horse in swiss alps"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                type="submit"
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-20">
+              <Loader />
+            </div>
+          )}
+
+          {images.length === 0 && !isLoading && (
+            <Empty label="No images generated." />
+          )}
+          <div className="flex items-center justify-center w-full  mt-8">
+            {images.map((image) => (
+              <Card key={image} className="rounded-lg overflow-hidden w-96">
+                <div className="relative aspect-square">
+                  <Image alt="image" src={image} fill />
+                </div>
+                <CardFooter className="p-2">
+                  <Button
+                    onClick={() => window.open(image)}
+                    variant={"secondary"}
+                    className="w-full"
+                  >
+                    <Download className="h-4 w-4 mr-2" />
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImagePage;
---
+++ app/api/image/route.ts
@@ -0,0 +1,47 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { OpenAI } from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_PARIK_APIKEY,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = "1", resolution = "521x512" } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!openai.apiKey) {
+      return new NextResponse("OpenAI API Key is not configured", {
+        status: 500,
+      });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Prompt are required", { status: 400 });
+    }
+    if (!amount) {
+      return new NextResponse("Amount are required", { status: 400 });
+    }
+    if (!resolution) {
+      return new NextResponse("Resolution are required", { status: 400 });
+    }
+
+    const response = await openai.images.generate({
+      model: "dall-e-3",
+      prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.log("[IMAGE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
